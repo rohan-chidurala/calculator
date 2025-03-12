770649b63a6f91c2bc0a77fc8d4f5ad97f025276
Rohan Chidurala <59586662+rohan-chidurala@users.noreply.github.com>
2025-03-11 22:02:47 -0700

    implemented subtract

--- calculator.c
+++ calculator.c
@@ -1,4 +1,9 @@
 #include "calculator.h"
+
 int add(int a, int b) {
-return a + b;
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
 }
--- calculator.h
+++ calculator.h
@@ -1,4 +1,5 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 int add(int a, int b);
+int subtract(int a, int b);
 #endif
